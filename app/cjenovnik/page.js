44d886537c9f13647c352c336e63679018dd54bd
extract36111 <digitalartefakt361@gmail.com>
2025-08-02 09:42:20 +0200

    cjenovnik

--- app/cjenovnik/page.js
+++ app/cjenovnik/page.js
@@ -82,7 +82,7 @@ export default function Cjenovnik_Page() {
                         </div>
                         <div className="single-service-style1__img">
                           <div className="single-service-style1__inner">
-                            <img src="/assets/images/resources/Mirav-2096_compressed.webp" alt="Crveni Golf 8" />
+                            <img src="/assets/images/resources/crveni-golf.jpg" alt="Crveni Golf 8" />
                           </div>
                           <div className="single-service-style1__text">
                             <ul className="price-list">
