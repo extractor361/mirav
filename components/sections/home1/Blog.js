ef91a58097c2eff5da3a11bc16fb599605e6367f
extract36111 <digitalartefakt361@gmail.com>
2025-08-12 16:19:14 +0200

    blog naslovna

--- components/sections/home1/Blog.js
+++ components/sections/home1/Blog.js
@@ -1,200 +1,119 @@
 'use client'
-import React from 'react'
-import Link from "next/link"
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 export default function Blog() {
-    return (
-        <> 
+  const [novosti, setNovosti] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-            <section className="blog-style1">
-                <div className="container">
-                    <div className="sec-title text-center">
-                        <div className="sub-title">
-                            <h4>Objave iz bloga</h4>
-                        </div>
-                        <h2>Poslednje objave iz bloga</h2>
+  useEffect(() => {
+    fetch('https://miravapibackend.online/api/dohvatiNovosti')  // pretpostavljam da je tvoj API dostupan na ovom endpointu
+      .then(res => {
+        if (!res.ok) throw new Error('Greška pri dohvatanju novosti')
+        return res.json()
+      })
+      .then(data => {
+        setNovosti(data)
+        setLoading(false)
+      })
+      .catch(err => {
+        setError(err.message)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <p>Učitavanje...</p>
+  if (error) return <p>Greška: {error}</p>
+
+  return (
+    <>
+      <section className="blog-style1">
+        <div className="container">
+          <div className="sec-title text-center">
+            <div className="sub-title">
+              <h4>Objave iz bloga</h4>
+            </div>
+            <h2>Poslednje objave iz bloga</h2>
+          </div>
+          <div className="row">
+            {novosti.map(({ novosti_id, naslov, sadrzaj, slika, datum_objave, korisnik_korisnik_id }, index) => (
+              <div key={novosti_id} className="col-xl-4 col-lg-6 col-md-6">
+                <div className="single-blog-style1">
+                  <div className="single-blog-style1__img">
+                    <div className="inner">
+                      {slika ? (
+                        <>
+                          <img src={`/slike/${slika}`} alt={naslov} />
+                          <div className="single-blog-style1__overlay-icon">
+                            <Link className="lightbox-image" data-fancybox="gallery" href={`/uploads/${slika}`}>
+                              <i className="icon-zoom"></i>
+                            </Link>
+                          </div>
+                        </>
+                      ) : (
+                        <img src="../assets/images/blog/default-image.jpg" alt="Default" />
+                      )}
                     </div>
-                    <div className="row">
-                        
-                        <div className="col-xl-4 col-lg-6 col-md-6">
-                            <div className="single-blog-style1">
-                                <div className="single-blog-style1__img">
-                                    <div className="inner">
-                                        <img src="assets/images/blog/blog-v1-1.jpg" alt="Awesome Image"/>
-                                        <div className="single-blog-style1__overlay-icon">
-                                            <Link className="lightbox-image" data-fancybox="gallery"
-                                                href="assets/images/blog/blog-v1-1.jpg">
-                                                <i className="icon-zoom"></i>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                    <div className="single-blog-style1__overlay-content">
-                                        <div className="date-box">
-                                            <h6>07. jul, 2025.</h6>
-                                        </div>
-                                        <div className="category">
-                                            <h5>Savjeti za vozače</h5>
-                                        </div>
-                                        <div className="meta-box">
-                                            <ul className="meta-info clearfix">
-                                                <li>
-                                                    <div className="icon">
-                                                        <i className="icon-user"></i>
-                                                    </div>
-                                                    <div className="text">
-                                                        <p>Mirjana</p>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div className="icon">
-                                                        <i className="icon-stopwatch"></i>
-                                                    </div>
-                                                    <div className="text">
-                                                        <p>3 Minuta čitanja</p>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="single-blog-style1__title">
-                                    <h3><Link href="/">5 najčešćih grešaka koje 
-                                            kandidati prave.</Link></h3>
-                                    <div className="btn-box">
-                                        <Link className="thm-btn3" href="/">
-                                            <i className="icon-next"></i>Pročitaj više
-                                        </Link>
-                                    </div>
-                                </div>
-
+                    <div className="single-blog-style1__overlay-content">
+                      <div className="date-box">
+                        <h6>{new Date(datum_objave).toLocaleDateString('sr-RS', { day: '2-digit', month: 'long', year: 'numeric' })}</h6>
+                      </div>
+                      <div className="category">
+                        <h5>Savjeti za vozače</h5> {/* možeš napraviti polje kategorije ako želiš */}
+                      </div>
+                      <div className="meta-box">
+                        <ul className="meta-info clearfix">
+                          <li>
+                            <div className="icon">
+                              <i className="icon-user"></i>
                             </div>
-                        </div>
-                        
-                        <div className="col-xl-4 col-lg-6 col-md-6">
-                            <div className="single-blog-style1">
-                                <div className="single-blog-style1__img">
-                                    <div className="inner">
-                                        <img src="assets/images/blog/blog-v1-2.jpg" alt="Awesome Image"/>
-                                        <div className="single-blog-style1__overlay-icon">
-                                            <Link className="lightbox-image" data-fancybox="gallery"
-                                                href="assets/images/blog/blog-v1-2.jpg">
-                                                <i className="icon-zoom"></i>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                    <div className="single-blog-style1__overlay-content">
-                                        <div className="date-box">
-                                            <h6>07. jul, 2025.</h6>
-                                        </div>
-                                        <div className="category">
-                                            <h5>Savjeti za vozače</h5>
-                                        </div>
-                                        <div className="meta-box">
-                                            <ul className="meta-info clearfix">
-                                                <li>
-                                                    <div className="icon">
-                                                        <i className="icon-user"></i>
-                                                    </div>
-                                                    <div className="text">
-                                                        <p>Mirjana</p>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div className="icon">
-                                                        <i className="icon-stopwatch"></i>
-                                                    </div>
-                                                    <div className="text">
-                                                        <p>1 minut čitanja</p>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="single-blog-style1__title">
-                                    <h3><Link href="/">Polaganje vozačkog u Crnoj Gori: </Link></h3>
-                                    <div className="btn-box">
-                                        <Link className="thm-btn3" href="/">
-                                            <i className="icon-next"></i>Pročitaj više
-                                        </Link>
-                                    </div>
-                                </div>
-
+                            <div className="text">
+                              <p>Autor: Admin</p> {/* Ako imaš imena autora, ubaci ih ovde */}
                             </div>
-                        </div>
-                        
-                        <div className="col-xl-4 col-lg-6 col-md-6">
-                            <div className="single-blog-style1">
-                                <div className="single-blog-style1__img">
-                                    <div className="inner">
-                                        <img src="assets/images/blog/blog-v1-3.jpg" alt="Awesome Image"/>
-                                        <div className="single-blog-style1__overlay-icon">
-                                            <Link className="lightbox-image" data-fancybox="gallery"
-                                                href="assets/images/blog/blog-v1-3.jpg">
-                                                <i className="icon-zoom"></i>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                    <div className="single-blog-style1__overlay-content">
-                                        <div className="date-box">
-                                            <h6>07 jul, 2025.</h6>
-                                        </div>
-                                        <div className="category">
-                                            <h5>Savjeti za vozače</h5>
-                                        </div>
-                                        <div className="meta-box">
-                                            <ul className="meta-info clearfix">
-                                                <li>
-                                                    <div className="icon">
-                                                        <i className="icon-user"></i>
-                                                    </div>
-                                                    <div className="text">
-                                                        <p>miladin</p>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div className="icon">
-                                                        <i className="icon-stopwatch"></i>
-                                                    </div>
-                                                    <div className="text">
-                                                        <p>4 minuta čitanja</p>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="single-blog-style1__title">
-                                    <h3><Link href="/">Automatski ili manuelni mjenjač 
-                                                    – šta je bolje za početnike?</Link></h3>
-                                    <div className="btn-box">
-                                        <Link className="thm-btn3" href="/">
-                                            <i className="icon-next"></i>Pročitaj više
-                                        </Link>
-                                    </div>
-                                </div>
+                          </li>
+                          <li>
+                            <div className="icon">
+                              <i className="icon-stopwatch"></i>
                             </div>
-                        </div>
-
+                            <div className="text">
+                              <p>2 minuta čitanja</p> {/* Možeš dodati proračun ako imaš broj reči */}
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
-
-                    <div className="section-btn">
-                        <div className="section-btn-inner">
-                            <div className="section-btn-inner-bg"
-                                style={{ backgroundImage: "url(assets/images/pattern/thm-pattern-1.png)" }}></div>
-                            <p>
-                                Želiš da saznaš više?
-                                <Link href="/blog-2"><i className="icon-next"></i>pročitaj još</Link>
-                            </p>
-                        </div>
+                  </div>
+                  <div className="single-blog-style1__title">
+                    <h3>
+                      <Link href={`/blog/${novosti_id}`}>{naslov}</Link>
+                    </h3>
+                    <div className="btn-box">
+                      <Link className="thm-btn3" href={`/blog/${novosti_id}`}>
+                        <i className="icon-next"></i> Pročitaj više
+                      </Link>
                     </div>
-
-
+                  </div>
                 </div>
-            </section>
-
-        </>
-    )
+              </div>
+            ))}
+          </div>
+          <div className="section-btn">
+            <div className="section-btn-inner">
+              <div
+                className="section-btn-inner-bg"
+                style={{ backgroundImage: "url(assets/images/pattern/thm-pattern-1.png)" }}
+              ></div>
+              <p>
+                Želiš da saznaš više?
+                <Link href="/blog-2">
+                  <i className="icon-next"></i> pročitaj još
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
