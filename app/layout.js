0bdbfd9e01b8cdf22f637600923a0814275facc7
extractor361 <selastan@gmail.com>
2026-02-10 20:38:53 +0100

    seo

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,19 @@ import "@/node_modules/react-modal-video/css/modal-video.css";
 import 'swiper/css';
 import "swiper/css/pagination";
 import 'swiper/css/free-mode';
-
+import "/assets/css/style.css";
+import "/assets/css/responsive.css";
 
 
 export default function RootLayout({ children }) {
-     return (
-    <html lang="en">
-      <body className="body-bg-1">
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                {/* CSS fajlovi iz public foldera */}
+                <link rel="stylesheet" href="/assets/css/style.css" />
+                <link rel="stylesheet" href="/assets/css/responsive.css" />
+            </head>
+            <body className="body-bg-1">{children}</body>
+        </html>
+    )
 }
