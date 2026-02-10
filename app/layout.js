8524d7b7fee28c8312d916285111c9c3d9c8ec59
extractor361 <selastan@gmail.com>
2026-02-10 20:40:09 +0100

    seo

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,7 @@ import "@/node_modules/react-modal-video/css/modal-video.css";
 import 'swiper/css';
 import "swiper/css/pagination";
 import 'swiper/css/free-mode';
-import "/assets/css/style.css";
-import "/assets/css/responsive.css";
+
 
 
 export default function RootLayout({ children }) {
